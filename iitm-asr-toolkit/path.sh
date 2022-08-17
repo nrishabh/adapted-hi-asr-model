f1fded77a45ad163fad09bc320215e12b8cbbc81
nrishabh <rishabh.nanawati7@gmail.com>
2022-08-17 08:00:29 +0000

    added iitm-asr transcription files

--- iitm-asr-toolkit/path.sh
+++ iitm-asr-toolkit/path.sh
@@ -6,7 +6,18 @@ KALDI_ROOT=/workspaces/adapted-hi-asr-model/kaldi-toolkit
 export PATH=$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:$PWD:$PATH
 [ ! -f $KALDI_ROOT/tools/config/common_path.sh ] && echo >&2 "The standard file $KALDI_ROOT/tools/config/common_path.sh is not present -> Exit!" && exit 1
 . $KALDI_ROOT/tools/config/common_path.sh
-export LD_LIBRARY_PATH=/usr/lib32/atlas:/usr/local/cuda/lib64:$LD_LIBRARY_PATH
+
+LD_LIBRARY_PATH=/usr/lib32/atlas
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$KALDI_ROOT/tools/openfst/lib
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$KALDI_ROOT/tools/openfst-1.7.2/lib
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$KALDI_ROOT/src/base
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$KALDI_ROOT/src/fstext
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$KALDI_ROOT/src/gmm
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$KALDI_ROOT/src/lm
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$KALDI_ROOT/src/matrix
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$KALDI_ROOT/src/util
+
+export LD_LIBRARY_PATH
 export LC_ALL=C
 
 
--- iitm-asr-toolkit/trim_dataset.py
+++ iitm-asr-toolkit/trim_dataset.py
@@ -9,25 +9,27 @@ AUDIO_FOLDER = os.getcwd()+r"/audio/"+sys.argv[1]+r"/"
 DATA_FOLDER = os.getcwd()+r"/data/"+sys.argv[1]+r"/"
 
 audio_files = os.listdir(AUDIO_FOLDER)
-transcription_files = ["segments", "spk2utt", "text", "utt2spk"]
+transcription_files = os.listdir(DATA_FOLDER)
+excluded_files = ["README", "wav.scp"]
 
 print("Trimming dataset based on available audio files...")
 for filename in transcription_files:
-    
-    data = list()
-    
-    with open(DATA_FOLDER+filename, mode='r', encoding='utf-8-sig') as f:
-        for line in f.read().split("\n"):
-            if (line[:21]+".wav") in audio_files:
-                data.append(line)
-    
-    os.rename(DATA_FOLDER+filename, DATA_FOLDER+"preTrimmed_"+filename)
+    if filename not in excluded_files:
+        data = list()
+        
+        with open(DATA_FOLDER+filename, mode='r', encoding='utf-8-sig') as f:
+            for line in f.read().split("\n"):
+                if (line[:21]+".wav") in audio_files:
+                    data.append(line)
+        
+        os.rename(DATA_FOLDER+filename, DATA_FOLDER+"preTrimmed_"+filename)
 
-    with open(DATA_FOLDER+filename, mode='w', encoding='utf-8-sig') as f:
-        for line in data:
-            f.write(line+"\n")
-    
-    print("DONE: "+filename)
+        with open(DATA_FOLDER+filename, mode='w', encoding='utf-8-sig') as f:
+            for line in data[:-1]:
+                f.write(line+"\n")
+            f.write(data[-1])
+        
+        print("DONE: "+filename)
 
 os.rename(DATA_FOLDER+"wav.scp", DATA_FOLDER+"preTrimmed_"+"wav.scp")
 
