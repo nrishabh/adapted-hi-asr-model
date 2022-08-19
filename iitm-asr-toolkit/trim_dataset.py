56f322bcc966aff6a369c515cd40e3064ddbe7fa
nrishabh <rishabh.nanawati7@gmail.com>
2022-08-19 05:46:26 +0000

    created utils ipynb

--- iitm-asr-toolkit/trim_dataset.py
+++ iitm-asr-toolkit/trim_dataset.py
@@ -1,5 +1,7 @@
 import os
 import sys
+import string
+import random
 
 # USAGE: python trim_dataset.py FOLDER_NAME
 # EXAMPLE: python trim_dataset.py train
@@ -9,32 +11,25 @@ AUDIO_FOLDER = os.getcwd()+r"/audio/"+sys.argv[1]+r"/"
 DATA_FOLDER = os.getcwd()+r"/data/"+sys.argv[1]+r"/"
 
 audio_files = os.listdir(AUDIO_FOLDER)
-transcription_files = os.listdir(DATA_FOLDER)
-excluded_files = ["README", "wav.scp"]
+transcription_files = os.listdir(DATA_FOLDER+r"preTrimmed/")
 
 print("Trimming dataset based on available audio files...")
 for filename in transcription_files:
-    if filename not in excluded_files:
-        data = list()
-        
-        with open(DATA_FOLDER+filename, mode='r', encoding='utf-8-sig') as f:
-            for line in f.read().split("\n"):
-                if (line[:21]+".wav") in audio_files:
-                    data.append(line)
-        
-        os.rename(DATA_FOLDER+filename, DATA_FOLDER+"preTrimmed_"+filename)
+    data = list()
+    
+    with open(DATA_FOLDER+r"/preTrimmed/"+filename, mode='r', encoding='utf-8-sig') as f:
+        for line in f.read().split("\n"):
+            if (line[:21]+".wav") in audio_files:
+                data.append(line)
 
-        with open(DATA_FOLDER+filename, mode='w', encoding='utf-8-sig') as f:
-            for line in data[:-1]:
-                f.write(line+"\n")
-            f.write(data[-1])
-        
-        print("DONE: "+filename)
-
-os.rename(DATA_FOLDER+"wav.scp", DATA_FOLDER+"preTrimmed_"+"wav.scp")
+    with open(DATA_FOLDER+filename, mode='w', encoding='utf-8-sig') as f:
+        for line in sorted(data):
+            f.write(line+"\n")
+    
+    print("DONE: "+filename)
 
 with open(DATA_FOLDER+"wav.scp", mode='w', encoding='utf-8-sig') as f:
-    for filename in audio_files:
+    for filename in sorted(audio_files):
         f.write(filename[:-4]+"\t"+AUDIO_FOLDER+filename+"\n")
 
 print("DONE: wav.scp")
